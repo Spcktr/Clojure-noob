847aafa0df7096773f19231bf750080cee137331
Spcktr <Spcktr@users.noreply.github.com>
2018-05-27 09:22:25 +1000

    Simple fruit list

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -9,4 +9,17 @@
         "By Zues' Hammer!")
     (do (println "Failure!")
         "By Aquaman's trident!"))
+
+  (let [fruit-count {:apple 2 :banana 12 :orange 5 :lemon 6
+                      :watermelon 1 :plums 0 :lime 40}
+        fruit-cost {:apple 1.00 :banana 0.05 :orange 4.00 :lemon 3.00
+                      :watermelon 7.00 :plums 3.00 :lime 0.50}
+        total-costs (reduce (fn [m f]
+                                (assoc m f  (* (fruit-count f) (fruit-cost f))))
+                                {}
+                              (keys fruit-count))
+        result {:fruit-counts fruit-count
+                :fruit-costs fruit-cost
+                :total-costs total-costs}]
+    result) 
   )
