159bdc736759762c0b4af656a9d1509a061e0a7c
Spcktr <Spcktr@users.noreply.github.com>
2018-07-23 21:02:24 +1000

    added arity

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -1,12 +1,14 @@
 (ns clojure-noob.core
-  (:gen-class))
+  (:gen-class)
+  (:use x.y.blah :as blah)
+  (:require '[clojure.repl :refer [doc]]))
 
 (defn -main
   [& args]
 
   (let [fruit-count {:apple 2 :banana 12 :orange 5 :lemon 6
                       :watermelon 1 :plums 0 :lime 40}
-        fruit-cost {:apple 1.00 :banana 0.05 :orange 4.00 :lemon 3.00
+        fruit-cost {:apple 2.00 :banana 0.05 :orange 4.00 :lemon 3.00
                       :watermelon 7.00 :plums 3.00 :lime 0.50}
         total-costs (reduce (fn [m f]
                                 (assoc m f  (* (fruit-count f) (fruit-cost f))))
@@ -17,3 +19,29 @@
                 :total-costs total-costs}]
     result)
   )
+
+(def my-mult
+  (fn [x y]
+    (* x y)))
+
+; arity
+(defn square-or-multi
+  "squares a single, multiplies 2"
+  ([] 0)
+  ([x] (* x x))
+  ([x y] (* x y)))
+
+; variable arity
+(defn add-arg-count
+  "returns first arg + the number of aditional args"
+  [first & more]
+  (+ first (count more)))
+
+
+(defn weather-judge
+ "given a temp in degrees C, comments on weather"
+ [temp]
+ (cond
+ (< temp 20) "its cold"
+ (> temp 20) "its hot"
+ :else "it's comfortable"))
